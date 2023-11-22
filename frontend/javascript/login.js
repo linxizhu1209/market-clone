d27c33747e6053f452b007964a624a3a7d5c5da7
linxizhu1209 <dlahj1209@naver.com>
2023-11-22 17:28:11 +0900

    feat: 로그인 인증 기능 추가
    로그인 인증이 완료되면 당근마켓 클론코딩 아이템 목록이 보이게 기능을 추가하였습니다.

---
+++ frontend/javascript/login.js
@@ -0,0 +1,43 @@
+const form = document.querySelector("#login-form");
+
+const handleSubmit = async (event) => {
+  event.preventDefault();
+  const formData = new FormData(form);
+  const sha256Password = sha256(formData.get("password"));
+  formData.set("password", sha256Password);
+
+  const res = await fetch("/login", {
+    method: "POST",
+    body: formData,
+  });
+  const data = await res.json();
+  const accessToken = data.access_token;
+  window.localStorage.setItem("token", accessToken);
+  //   window.sessionStorage.setItem("token", accessToken);
+  alert("로그인되었습니다!");
+
+  window.location.pathname = "/";
+
+  const btn = document.createElement("button");
+  btn.innerText = "상품가져오기";
+  btn.addEventListener("click", async () => {
+    const res = await fetch("/items", {
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+    });
+    const data = await res.json();
+    console.log(data);
+  });
+  infoDiv.appendChild(btn);
+
+  //   if (res.status === 200) {
+  //     alert("로그인에 성공했습니다!");
+  //     window.location.pathname = "/";
+  //     console.log("액세스토큰!! : ", data);
+  //   } else if (res.status === 401) {
+  //     alert("id혹은 패스워드가 틀렸습니다!!");
+  //   }
+};
+
+form.addEventListener("submit", handleSubmit);
--- main.py
+++ main.py
@@ -1,9 +1,13 @@
-from fastapi import FastAPI,UploadFile,Form,Response
+from fastapi import FastAPI,UploadFile,Form,Response,Depends
 from fastapi.staticfiles import StaticFiles
 from fastapi.encoders import jsonable_encoder
 from fastapi.responses import JSONResponse
 from typing import Annotated
+from fastapi_login import LoginManager
+from fastapi_login.exceptions import InvalidCredentialsException
 import sqlite3
+
+
 
 con = sqlite3.connect('items.db',check_same_thread=False)
 cur = con.cursor()
@@ -22,6 +26,63 @@ cur.execute(f"""
 """)
 
 app = FastAPI()
+
+SECRET = "super-coding"
+manager = LoginManager(SECRET,'/login')
+
+# //로그인 매니저가 쿼리를 조회할때 키를 함께 조회함
+@manager.user_loader()
+def query_user(data):
+    WHERE_STATEMENTS = f'id="{data}"'
+    if type(data) == dict:
+        WHERE_STATEMENTS = f'''id="{data['id']}"'''
+    
+    con.row_factory =sqlite3.Row
+    cur=con.cursor()
+    user = cur.execute(f"""
+                       SELECT * from users WHERE {WHERE_STATEMENTS}
+                       """).fetchone()
+    return user
+
+@app.post('/login')
+def login(id:Annotated[str,Form()],
+          password:Annotated[str,Form()]):
+     
+    user = query_user(id)
+    if not user:
+        raise InvalidCredentialsException
+    # status를 자동으로 내려주게됨
+    elif password != user["password"]:
+        # user["password"]로 했더니 에러(tuple) 나서 인덱스값으로 변경해줌 -> 컬럼명을 같이 가져오지 않았기때문임! con.row_factory =sqlite3.Row 
+        raise InvalidCredentialsException
+    # 파이썬에선s raise를 통해 에러메시지 던짐
+    # 파이썬에선 else if = elif
+
+    access_token = manager.create_access_token(data={
+        'sub': {
+        'id':user['id'],
+        'name':user['name'],
+        'email':user['email']        
+        }
+    })
+    
+    
+    return {'access_token':access_token}
+
+
+
+
+@app.post('/signup')
+def signup(id:Annotated[str,Form()],password:Annotated[str,Form()],
+           name:Annotated[str,Form()],
+           email:Annotated[str,Form()]):
+    cur.execute(f"""
+                INSERT INTO users(id,name,email,password)
+                VALUES ('{id}','{name}','{email}','{password}')
+                """)
+    con.commit()
+    return '200'
+
 
 
 
@@ -43,7 +104,7 @@ async def create_item(image:UploadFile,
 
 
 @app.get('/items')
-async def get_items():
+async def get_items(user=Depends(manager)):
     #컬럼명도 같이 가져옴
     con.row_factory =sqlite3.Row
     cur = con.cursor()
@@ -63,18 +124,6 @@ async def get_image(item_id):
                               """).fetchone()[0]
     return Response(content=bytes.fromhex(image_bytes), media_type='image/*')
 
-
-
-@app.post('/signup')
-def signup(id:Annotated[str,Form()],password:Annotated[str,Form()],
-           name:Annotated[str,Form()],
-           email:Annotated[str,Form()]):
-    cur.execute(f"""
-                INSERT INTO users(id,name,email,password)
-                VALUES ('{id}','{name}','{email}','{password}')
-                """)
-    con.commit()
-    return '200'
 
 
 
